af591a2ae4606b7720880fee9a9022ecb441af2c
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-10-02 16:12:04 +0800

    Add section check

--- run.R
+++ run.R
@@ -5,11 +5,12 @@
 library(data.table)
 library(here)
 library(pagedown)
+library(tidyverse)
 
 just_copy <- function(input_file, out_file, append = TRUE) {
-    message("Copying contents of ", input_file, " to ", out_file)
-    fl_content <- readLines(input_file)
-    write_lines(fl_content, out_file, append = TRUE)
+  message("Copying contents of ", input_file, " to ", out_file)
+  fl_content <- readLines(input_file)
+  write_lines(fl_content, out_file, append = TRUE)
 }
 
 position_path <- "positions.csv"
@@ -17,12 +18,22 @@ profile_path <- "profile.csv"
 aside_md <- "aside.md"
 intro_md <- "intro.md"
 
+# Load csv with position info
+position_data <- read_csv(position_path) %>%
+  mutate_all(fill_nas) %>%
+  arrange(order, desc(end)) %>%
+  mutate(id = 1:n()) %>%
+  nest(data = c(-id, -section))
+sections <- unique(position_data$section)
+rm(position_data)
+
 # Load csv with profile info
 profile <- fread(profile_path)
 
 # Copy html and set output
 file.copy(file.path(here(), "template/styles.css"), "styles.css")
-out_file = "index.Rmd"
+out_file <- "index.Rmd"
+
 
 
 # Part 1: yaml ---------------------------------------------------------
@@ -41,7 +52,7 @@ input_file <- file.path(here(), "template/template_03_aside.Rmd")
 message("Copying contents of ", input_file, " to ", out_file)
 fl_content <- readLines(input_file)
 if (!is.na(profile$Logo)) {
-    fl_content <- gsub("logo.jpg", profile$Logo, fl_content)
+  fl_content <- gsub("logo.jpg", profile$Logo, fl_content)
 }
 write_lines(fl_content, out_file, append = TRUE)
 
@@ -55,64 +66,86 @@ just_copy(input_file, out_file)
 # Copy intro.md
 just_copy(intro_md, out_file)
 
-input_file <- file.path(here(), "template/template_04_research_experience.Rmd")
-just_copy(input_file, out_file)
-input_file <- file.path(here(), "template/template_04_education.Rmd")
-just_copy(input_file, out_file)
-input_file <- file.path(here(), "template/template_04_certificate.Rmd")
-just_copy(input_file, out_file)
-input_file <- file.path(here(), "template/template_04_teaching_positions.Rmd")
-just_copy(input_file, out_file)
-input_file <- file.path(here(), "template/template_04_grant.Rmd")
-just_copy(input_file, out_file)
-input_file <- file.path(here(), "template/template_04_book_chapters.Rmd")
-just_copy(input_file, out_file)
-input_file <- file.path(here(), "template/template_04_academic_articles.Rmd")
-message("Copying contents of ", input_file, " to ", out_file)
-fl_content <- readLines(input_file)
-id <- profile$Scholar_id
-if (!is.na(id)) {
+if ("research_positions" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_research_experience.Rmd")
+  just_copy(input_file, out_file)
+}
+
+if ("education" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_education.Rmd")
+  just_copy(input_file, out_file)
+}
+
+if ("certificate" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_certificate.Rmd")
+  just_copy(input_file, out_file)
+}
+
+if ("teaching_positions" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_teaching_positions.Rmd")
+  just_copy(input_file, out_file)
+}
+
+if ("grant" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_grant.Rmd")
+  just_copy(input_file, out_file)
+}
+
+if ("book_chapters" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_book_chapters.Rmd")
+  just_copy(input_file, out_file)
+}
+
+if ("academic_articles" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_academic_articles.Rmd")
+  message("Copying contents of ", input_file, " to ", out_file)
+  fl_content <- readLines(input_file)
+  id <- profile$Scholar_id
+  if (!is.na(id)) {
     source(file.path(here(), "template/citation.R"))
     total_cites <- print_cite_png(id)
     profile$total_cites <- total_cites
     profile$h_index <- ifelse(is.na(profile$`H-index`), 0, profile$`H-index`)
     profile$i10_index <- ifelse(is.na(profile$`I10-index`), 0, profile$`I10-index`)
-} else {
+  } else {
     # No citation info available
     # Delete related rows
     del_i <- which(grepl(":::", fl_content))
     fl_content <- fl_content[-c(del_i[1]:del_i[2])]
+  }
+  write_lines(fl_content, out_file, append = TRUE)
 }
-write_lines(fl_content, out_file, append = TRUE)
-input_file <- file.path(here(), "template/template_04_presentation.Rmd")
-just_copy(input_file, out_file)
+
+if ("presentation" %in% sections) {
+  input_file <- file.path(here(), "template/template_04_presentation.Rmd")
+  just_copy(input_file, out_file)
+}
 
 # Render ------------------------------------------------------------------
 rmarkdown::render("index.Rmd")
 tryCatch(
-    pagedown::chrome_print("index.html", "index.pdf"),
-    error = function(e) {
-        message(e$message)
-        message("PDF cannot be generated, please print pdf by your own.")
-    }
+  pagedown::chrome_print("index.html", "index.pdf"),
+  error = function(e) {
+    message(e$message)
+    message("PDF cannot be generated, please print pdf by your own.")
+  }
 )
 
 # Generate result directory
 if (!dir.exists("cv")) {
-    dir.create("cv")
+  dir.create("cv")
 }
 
 all_files <- c(
-    "styles.css",
-    "index.Rmd",
-    "index.html",
-    "index.pdf",
-    "citation.png")
-suppressWarnings(
-    {
-        file.copy(all_files, to = "cv")
-        file.remove(all_files)
-    }
+  "styles.css",
+  "index.Rmd",
+  "index.html",
+  "index.pdf",
+  "citation.png"
 )
+suppressWarnings({
+  file.copy(all_files, to = "cv")
+  file.remove(all_files)
+})
 
 message("Open index.html in cv directory to check your CV!")
--- template/citation.R
+++ template/citation.R
@@ -1,3 +1,4 @@
+# Code written by Guangchuang Yu, modified by Shixiang
 library(ggplot2)
 library(tinyscholar)
 library(ggstance)
@@ -8,27 +9,31 @@ library(ggimage)
 # id <- 'DO5oG40AAAAJ'
 
 print_cite_png <- function(id, out = "citation.png") {
-    citation <- tinyscholar::tinyscholar(id)$citation
-    citation <- citation[-1, ] # remove 'total' row
-    names(citation) <- c("year", "cites")
-    citation$year <- as.numeric(citation$year)
+  citation <- tinyscholar::tinyscholar(id)$citation
+  citation <- citation[-1, ] # remove 'total' row
+  names(citation) <- c("year", "cites")
+  citation$year <- as.numeric(citation$year)
 
-    citation$year <- factor(citation$year)
+  citation$year <- factor(citation$year)
 
-    p <- ggplot(citation, aes(cites, year)) +
-        geom_barh(stat='identity', fill = "#96B56C") +
-        geom_text2(aes(label=cites, subset = cites > max(cites)/5), hjust=1.1, size=5) +
-        labs(caption = "data from Google Scholar") +
-        scale_x_continuous(position="top") +
-        theme_minimal(base_size=14) + xlab(NULL) + ylab(NULL) +
-        theme(panel.grid.major.y = element_blank(),
-              panel.grid.minor = element_blank(),
-              panel.grid.major.x = element_line(linetype="dashed"),
-              plot.caption=element_text(colour='grey30')) +
-        theme_transparent()
+  p <- ggplot(citation, aes(cites, year)) +
+    geom_barh(stat = "identity", fill = "#96B56C") +
+    geom_text2(aes(label = cites, subset = cites > max(cites) / 5), hjust = 1.1, size = 5) +
+    labs(caption = "data from Google Scholar") +
+    scale_x_continuous(position = "top") +
+    theme_minimal(base_size = 14) +
+    xlab(NULL) +
+    ylab(NULL) +
+    theme(
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.grid.major.x = element_line(linetype = "dashed"),
+      plot.caption = element_text(colour = "grey30")
+    ) +
+    theme_transparent()
 
-    ggsave(p, file = out, width=3.5, height=6, bg = "transparent")
-    return(sum(citation$cites))
+  ggsave(p, file = out, width = 3.5, height = 6, bg = "transparent")
+  return(sum(citation$cites))
 }
 
 ## library(magick)
