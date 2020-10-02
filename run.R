b3be3957707212a3577804788752872dcfb4b808
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-10-02 17:07:09 +0800

    Make run.R executable

--- run.R
+++ run.R
@@ -1,9 +1,13 @@
+#!/usr/bin/env Rscript
 # Create CV automatically
 # Author: Shixiang Wang
 # License: MIT
+template_path <- commandArgs(trailingOnly = TRUE)[1]
+if (is.na(template_path)) {
+    template_path <- "template"
+}
 
 library(data.table)
-library(here)
 library(pagedown)
 library(tidyverse)
 
@@ -19,11 +23,7 @@ aside_md <- "aside.md"
 intro_md <- "intro.md"
 
 # Load csv with position info
-position_data <- read_csv(position_path) %>%
-  mutate_all(fill_nas) %>%
-  arrange(order, desc(end)) %>%
-  mutate(id = 1:n()) %>%
-  nest(data = c(-id, -section))
+position_data <- read_csv(position_path)
 sections <- unique(position_data$section)
 rm(position_data)
 
@@ -31,30 +31,28 @@ rm(position_data)
 profile <- fread(profile_path)
 
 # Copy html and set output
-file.copy(file.path(here(), "template/styles.css"), "styles.css")
+file.copy(file.path(template_path, "styles.css"), "styles.css")
 out_file <- "index.Rmd"
 
-
-
 # Part 1: yaml ---------------------------------------------------------
-input_file <- file.path(here(), "template/template_01_yml.Rmd")
+input_file <- file.path(template_path, "template_01_yml.Rmd")
 message("Copying contents of ", input_file, " to ", out_file)
 fl_content <- readLines(input_file)
 fl_content <- gsub("Guangchuang Yu", profile$Name, fl_content)
 write_lines(fl_content, out_file)
 
 # Part 2: header ----------------------------------------------------------
-input_file <- file.path(here(), "template/template_02_header.Rmd")
+input_file <- file.path(template_path, "template_02_header.Rmd")
 just_copy(input_file, out_file)
 
 # Part 3: aside -----------------------------------------------------------
-input_file <- file.path(here(), "template/template_03_aside.Rmd")
+input_file <- file.path(template_path, "template_03_aside.Rmd")
 message("Copying contents of ", input_file, " to ", out_file)
 fl_content <- readLines(input_file)
 if (!is.na(profile$Logo)) {
   fl_content <- gsub("logo.png", profile$Logo, fl_content)
 } else {
-  file.copy(file.path(here(), "template/logo.png"), "logo.png")
+  file.copy(file.path(template_path, "logo.png"), "logo.png")
   #fl_content <- fl_content[!grepl("logo.png", fl_content)]
 }
 write_lines(fl_content, out_file, append = TRUE)
@@ -63,49 +61,49 @@ write_lines(fl_content, out_file, append = TRUE)
 just_copy(aside_md, out_file)
 
 # Part 4: main ------------------------------------------------------------
-input_file <- file.path(here(), "template/template_04_main.Rmd")
+input_file <- file.path(template_path, "template_04_main.Rmd")
 just_copy(input_file, out_file)
 
 # Copy intro.md
 just_copy(intro_md, out_file)
 
 if ("research_positions" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_research_experience.Rmd")
+  input_file <- file.path(template_path, "template_04_research_experience.Rmd")
   just_copy(input_file, out_file)
 }
 
 if ("education" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_education.Rmd")
+  input_file <- file.path(template_path, "template_04_education.Rmd")
   just_copy(input_file, out_file)
 }
 
 if ("certificate" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_certificate.Rmd")
+  input_file <- file.path(template_path, "template_04_certificate.Rmd")
   just_copy(input_file, out_file)
 }
 
 if ("teaching_positions" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_teaching_positions.Rmd")
+  input_file <- file.path(template_path, "template_04_teaching_positions.Rmd")
   just_copy(input_file, out_file)
 }
 
 if ("grant" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_grant.Rmd")
+  input_file <- file.path(template_path, "template_04_grant.Rmd")
   just_copy(input_file, out_file)
 }
 
 if ("book_chapters" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_book_chapters.Rmd")
+  input_file <- file.path(template_path, "template_04_book_chapters.Rmd")
   just_copy(input_file, out_file)
 }
 
 if ("academic_articles" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_academic_articles.Rmd")
+  input_file <- file.path(template_path, "template_04_academic_articles.Rmd")
   message("Copying contents of ", input_file, " to ", out_file)
   fl_content <- readLines(input_file)
   id <- profile$Scholar_id
   if (!is.na(id)) {
-    source(file.path(here(), "template/citation.R"))
+    source(file.path(template_path, "citation.R"))
     total_cites <- print_cite_png(id)
     profile$total_cites <- total_cites
     profile$h_index <- ifelse(is.na(profile$`H-index`), 0, profile$`H-index`)
@@ -120,7 +118,7 @@ if ("academic_articles" %in% sections) {
 }
 
 if ("presentation" %in% sections) {
-  input_file <- file.path(here(), "template/template_04_presentation.Rmd")
+  input_file <- file.path(template_path, "template_04_presentation.Rmd")
   just_copy(input_file, out_file)
 }
 
